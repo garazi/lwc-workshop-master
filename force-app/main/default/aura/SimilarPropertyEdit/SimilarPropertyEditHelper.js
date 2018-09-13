2bb9946d7282a4fb9694624f542ff77babcf4d62
garazi <grewis@grewis-ltm4.internal.salesforce.com>
2018-09-13 15:24:24 -0700

    cleanup

--- force-app/main/default/aura/SimilarPropertyEdit/SimilarPropertyEditHelper.js
+++
@@ -1,9 +0,0 @@
-({
-    showHideModal : function(component) {
-        var modal = component.find("editDialog");
-        $A.util.toggleClass(modal, 'slds-fade-in-open');
-        var overlay = component.find("overlay");
-        $A.util.toggleClass(overlay, 'slds-backdrop--open');
-        component.set("v.showDialog", "false");
-    }
-})
