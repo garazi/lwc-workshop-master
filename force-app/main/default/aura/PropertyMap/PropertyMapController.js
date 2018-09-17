725f363ec3793ed21b3bf9ea493241c8eaeb5945
garazi <grewis@grewis-ltm4.internal.salesforce.com>
2018-09-17 10:20:18 -0700

    more playing with map

--- force-app/main/default/aura/PropertyMap/PropertyMapController.js
+++ force-app/main/default/aura/PropertyMap/PropertyMapController.js
@@ -1,5 +1,6 @@
 ({
-	doInit: function(component) {
+	doInit: function(component,event,helper) {
+		console.log("doing init");
 		component.set("v.mapMarkers", [{
 			location: {
 				Street: component.get('v.property.Address__c'),
@@ -7,112 +8,50 @@
 				State: component.get('v.property.State__c')
 			},
 			title: component.get('v.property.Name'),
-			description: component.get('v.property.Description__c')
-            }]);
+			description: component.get('v.property.Title__c')
+			}]);
+		// helper.makeMap(component);
 	},
-	getProperties: function(component,event,helper) {
+	getProperties: function(component, event, helper) {
 		helper.getProps(component);
 	},
-	buildList : function (component) {
-		component.set('v.mapMarkers', [
-            {
-                location: {
-                    City: 'Cap-d\'Ail',
-                    Country: 'France',
-                },
-
-                icon: 'custom:custom26',
-                title: 'Cap-d\'Ail'
-            },
-            {
-                location: {
-                    City: 'Beaulieu-sur-Mer',
-                    Country: 'France',
-                },
-
-                icon: 'custom:custom96',
-                title: 'Beaulieu-sur-Mer'
-            },
-            {
-                location: {
-                    City: 'Saint-Jean-Cap-Ferrat',
-                    Country: 'France',
-                },
-
-                title: 'Saint-Jean-Cap-Ferrat'
-            },
-            {
-                location: {
-                    City: 'Villefranche-sur-Mer',
-                    Country: 'France',
-                },
-
-                icon: 'custom:custom92',
-                title: 'Villefranche-sur-Mer'
-            },
-            {
-                location: {
-                    City: 'Antibes',
-                    Country: 'France',
-                },
-
-                icon: 'custom:custom61',
-                title: 'Antibes'
-            },
-            {
-                location: {
-                    City: 'Juan-les-Pins',
-                    Country: 'France',
-                },
-
-                icon: 'custom:custom74',
-                title: 'Juan-les-Pins'
-            },
-            {
-                location: {
-                    City: 'Cannes',
-                    Country: 'France',
-                },
-
-                icon: 'custom:custom3',
-                title: 'Cannes'
-            },
-            {
-                location: {
-                    City: 'Saint-Raphaël',
-                    Country: 'France',
-                },
-
-                icon: 'custom:custom54',
-                title: 'Saint-Raphaël'
-            },
-            {
-                location: {
-                    City: 'Fréjus',
-                    Country: 'France',
-                },
-
-                icon: 'custom:custom88',
-                title: 'Fréjus'
-            },
-            {
-                location: {
-                    City: 'Sainte-Maxime',
-                    Country: 'France',
-                },
-
-                icon: 'custom:custom92',
-                title: 'Sainte-Maxime'
-            },
-            {
-                location: {
-                    City: 'Saint-Tropez',
-                    Country: 'France',
-                },
-
-                icon: 'custom:custom26',
-                title: 'Saint-Tropez'
-            },
-        ]);
-    }
+	buildList: function(component) {
+		console.log("fff: ", component.get("v.similarProperties"))
+		// component.set("v.mapMarker", [{
+		// 	location: {
+		// 		Street: component.get('v.property.Address__c'),
+		// 		City: component.get('v.property.City__c'),
+		// 		State: component.get('v.property.State__c')
+		// 	},
+		// 	title: component.get('v.property.Name'),
+		// 	description: component.get('v.property.Title__c')
+		// 	}]);
+		var similarProperties = component.get("v.similarProperties");
+		var propertiesArray = [];
+		var singleProperty = {
+			location: {
+				Street: component.get('v.property.Address__c'),
+				City: component.get('v.property.City__c'),
+				State: component.get('v.property.State__c')
+			},
+			title: component.get('v.property.Name'),
+			description: component.get('v.property.Title__c')
+		};
+		propertiesArray.push(singleProperty);
+		for (var i = 0; i < similarProperties.length; i++) {
+			singleProperty = {
+				location: {
+					Street: similarProperties[i].Address__c,
+					City: similarProperties[i].City__c,
+					State: similarProperties[i].State__c
+				},
+				title: similarProperties[i].Name,
+				description: similarProperties[i].Title__c
+			}
+			propertiesArray.push(singleProperty);
+		}
+		component.set("v.mapMarkers", propertiesArray);
+		console.log("list: ", propertiesArray)
+		helper.makeMap(component);
+	}
 })
--- force-app/main/default/aura/PropertyMap/PropertyMapHelper.js
+++ force-app/main/default/aura/PropertyMap/PropertyMapHelper.js
@@ -1,6 +1,6 @@
 ({
-    getProps : function (component) {
-        var action = component.get("c.getSimilarProperties");
+	getProps: function(component) {
+		var action = component.get("c.getMapMarkers");
 		action.setParams({
 			recordId: component.get("v.recordId"),
 			beds: component.get("v.property.Beds__c"),
@@ -8,111 +8,27 @@
 		});
 		action.setCallback(this, function(response) {
 			var similarProperties = response.getReturnValue();
-            console.log("Props: ", similarProperties);
-            component.set("v.similarProperties", similarProperties);
+			console.log("Props: ", similarProperties);
+			component.set("v.similarProperties", similarProperties);
 		});
 		$A.enqueueAction(action);
-    },
-    buildList : function (component, similarProperties) {
-        component.set('v.mapMarkers', [
-            {
-                location: {
-                    City: 'Cap-d\'Ail',
-                    Country: 'France',
-                },
-
-                icon: 'custom:custom26',
-                title: 'Cap-d\'Ail'
-            },
-            {
-                location: {
-                    City: 'Beaulieu-sur-Mer',
-                    Country: 'France',
-                },
-
-                icon: 'custom:custom96',
-                title: 'Beaulieu-sur-Mer'
-            },
-            {
-                location: {
-                    City: 'Saint-Jean-Cap-Ferrat',
-                    Country: 'France',
-                },
-
-                title: 'Saint-Jean-Cap-Ferrat'
-            },
-            {
-                location: {
-                    City: 'Villefranche-sur-Mer',
-                    Country: 'France',
-                },
-
-                icon: 'custom:custom92',
-                title: 'Villefranche-sur-Mer'
-            },
-            {
-                location: {
-                    City: 'Antibes',
-                    Country: 'France',
-                },
-
-                icon: 'custom:custom61',
-                title: 'Antibes'
-            },
-            {
-                location: {
-                    City: 'Juan-les-Pins',
-                    Country: 'France',
-                },
-
-                icon: 'custom:custom74',
-                title: 'Juan-les-Pins'
-            },
-            {
-                location: {
-                    City: 'Cannes',
-                    Country: 'France',
-                },
-
-                icon: 'custom:custom3',
-                title: 'Cannes'
-            },
-            {
-                location: {
-                    City: 'Saint-Raphaël',
-                    Country: 'France',
-                },
-
-                icon: 'custom:custom54',
-                title: 'Saint-Raphaël'
-            },
-            {
-                location: {
-                    City: 'Fréjus',
-                    Country: 'France',
-                },
-
-                icon: 'custom:custom88',
-                title: 'Fréjus'
-            },
-            {
-                location: {
-                    City: 'Sainte-Maxime',
-                    Country: 'France',
-                },
-
-                icon: 'custom:custom92',
-                title: 'Sainte-Maxime'
-            },
-            {
-                location: {
-                    City: 'Saint-Tropez',
-                    Country: 'France',
-                },
-
-                icon: 'custom:custom26',
-                title: 'Saint-Tropez'
-            },
-        ]);
-    }
+	},
+	makeMap: function(component) {
+		// var foo = component.find("googleMap");
+		// if (foo) {
+		// 	foo.destroy();
+		// }
+		$A.createComponent(
+			"lightning:map", {
+				"mapMarkers": component.get("v.mapMarkers"),
+				"zoomLevel": "13",
+                "aura:id": "googleMap"
+			},
+			function(newCmp) {
+				var body = component.get("v.body");
+				body.push(newCmp);
+				component.set("v.body", body);
+			}
+		)
+	}
 })
