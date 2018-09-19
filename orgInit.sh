8c20c5f03bf56cac47a1070ee35c5a13f9e38d4b
Greg Rewis <greg@assortedgarbage.com>
2018-09-19 11:39:48 -0700

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -1,6 +1,5 @@
 sfdx shane:org:create -f config/project-scratch-def.json -d 14 -s --json -u int -o work.shop
 sfdx force:source:push
-sfdx force:user:permset:assign -n CustomerID
+sfdx force:user:permset:assign -n AllAccess
 sfdx shane:user:password:set -l User -g User -p sfdx1234 --json
-sfdx force:apex:execute -f scripts/CustomerIDSetup.cls
 sfdx force:org:open
