23dd21bb0368182d19046a102a0921514b047b45
garazi <grewis@grewis-ltm4.internal.salesforce.com>
2019-04-21 00:05:59 -0700

    clean up

--- force-app/main/default/aura/BrokerDetails/BrokerDetailsHelper.js
+++
@@ -1,8 +0,0 @@
-({
-    showHide : function(component) {
-        var editForm = component.find("editForm");
-        $A.util.toggleClass(editForm, "slds-hide");
-        var viewForm = component.find("viewForm");
-		$A.util.toggleClass(viewForm, "slds-hide");
-    }
-})
